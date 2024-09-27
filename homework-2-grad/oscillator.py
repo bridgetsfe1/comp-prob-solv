a55083a6760bc2a32081ad50fb8c5b06bcd59ade
bridgetsfe1 <b.s.ferris@wustl.edu>
2024-09-27 04:03:39 +0000

    Finalize HW-2-grad

--- homework-2-grad/oscillator.py
+++ homework-2-grad/oscillator.py
@@ -16,16 +16,27 @@ dx = (L)/(n-1)
 grid = np.linspace(-L / 2, L / 2, n)
 
 #Potential Matrix (Harmonic and Anharmoic)
-# Potential Matrix (Harmonic and Anharmonic)
 def harmonic_matrix(grid):
+    """Computes the harmonic potential
+    Parameters:
+        grid: the size of the space in which potentials will be calculated
+    Returns:
+        list: Harmonic potentials over the grid space. 
+    """
     V_harmonic = 0.5 * m * omega**2 * (grid**2)
-    return np.diag(V_harmonic) 
+    return (V_harmonic) 
 
 def anharmonic_matrix(grid):
+    """Computes the anharmonic (Morse) potential
+    Parameters:
+        grid: the size of the space in which potentials will be calculated
+    Returns:
+        list: Harmonic potentials over the grid space. 
+    """
     x_0 = 0  # You can change this if needed
     anh_exp = np.exp(-beta * (grid - x_0))
     V_anharmonic = D * (1 - anh_exp)**2
-    return np.diag(V_anharmonic)  
+    return (V_anharmonic)  
 
 #Laplacian Matrix
 diagonal = -2 * np.ones(n) 
@@ -34,17 +45,21 @@ off_diagonal = np.ones(n-1)
 laplacian = (1/dx**2)*(np.diag(diagonal, k=0) + np.diag(off_diagonal, k=1) + np.diag(off_diagonal, k=-1))
 
 #Hamiltonian Matrix
-harmonic_V = harmonic_matrix(grid)
+harmonic_V = np.diag(harmonic_matrix(grid))
 def hamil():
+    """Computes the harmonic Hamiltonian
+    """
     hamiltonian_matrix = (-((h_bar**2) / (2 * m)) * laplacian) + harmonic_V
     return hamiltonian_matrix
 
-anharmonic_V = anharmonic_matrix(grid)
+anharmonic_V = np.diag(anharmonic_matrix(grid))
 def an_hamil():
+    """Computes the anharmonic Hamiltonian
+    """
     hamiltonian_matrix_morse = -((h_bar**2) / (2 * m)) * laplacian + anharmonic_V
     return hamiltonian_matrix_morse
 
-#Compute eigenvalues and eigenvectors
+#Compute and sort eigenvalues and eigenvectors (energies)
 eigenvalues_h, eigenvectors_h = np.linalg.eigh(hamil())
 sort = np.argsort(eigenvalues_h)
 eigenvalues_h = eigenvalues_h[sort]
@@ -55,31 +70,32 @@ eigenvalues_ah = eigenvalues_ah[sort]
 
 #Harmonic Graph                                          
 for i in range(10):                               
-    wavefunction_h= -eigenvectors_h[:, i]                                  
-    wavefunction_h= wavefunction_h/(np.sqrt(np.sum(wavefunction_h**2)*dx))    #Normalize Wavefunction
+    wavefunction_h= eigenvectors_h[:, i]                                  
     y=2*wavefunction_h+eigenvalues_h[i]                                     
-    plt.plot(grid,y, label=f"n= $\psi_{i+1}$")                               
-    plt.axvline(x=L/2)                  
-    plt.axvline(x=-L/2)
-    plt.hlines(eigenvalues_h[i],-L/2, L/2)               
+    plt.plot(grid,y, label=f"$\u03A8_{i+1}$: E = {eigenvalues_h[i]:.2f}")                             
+    plt.axvline(x=L/2, color = 'black')                  
+    plt.axvline(x=-L/2, color = 'black') 
+plt.plot(grid, harmonic_matrix(grid), color = "grey", linestyle= "--", label = "Harmonic Potential")            
 plt.title("First Ten Wavefunctions with Energy Levels")
-plt.xlabel("Length")
+plt.xlabel("Length (a.u.)")
 plt.ylabel("Energy (amu)")
+plt.ylim(0, 10)
 plt.legend()
 plt.savefig("First_10_harmonic")
 
 #Anharmonic Graph
+plt.clf()
 for i in range(10):                               
-    wavefunction_ah= -eigenvectors_ah[:, i]                                  
-    wavefunction_ah= wavefunction_ah/(np.sqrt(np.sum(wavefunction_ah**2)*dx))   
+    wavefunction_ah= eigenvectors_ah[:, i]                                  
     y=2*wavefunction_ah+eigenvalues_ah[i]                                     
-    plt.plot(grid,y, label=f"n= $\psi_{i+1}$")                               
-    plt.axvline(x=L/2)                  
-    plt.axvline(x=-L/2)
-    plt.hlines(eigenvalues_ah[i],-L/2, L/2)               
+    plt.plot(grid,y, label=f"$\u03A8_{i+1}$: E = {eigenvalues_ah[i]:.2f}")                               
+    plt.axvline(x=L/2, color = 'black')                  
+    plt.axvline(x=-L/2, color = 'black')   
+plt.plot(grid, anharmonic_matrix(grid), color = "grey", linestyle= "--", label = "Morse Potential")                       
 plt.title("First Ten Wavefunctions with Energy Levels")
-plt.xlabel("Length")
+plt.xlabel("Length (a.u.)")
 plt.ylabel("Energy (amu)")
+plt.ylim(0, 8)
 plt.legend()
 plt.savefig("First_10_anharmonic")
 
